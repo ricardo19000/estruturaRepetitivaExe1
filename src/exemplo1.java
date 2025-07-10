b123c0290a605ab350b41af08a0203d2cebbb03e
O Tal do Ricardo <33401083+ricardo19000@users.noreply.github.com>
2025-07-09 21:53:53 -0300

    Adição Exemplo 1 final

--- src/exemplo1.java
+++ src/exemplo1.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class exemplo1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Insira um valor");
+        int x = sc.nextInt();
+
+        int soma = 0;
+        while (x != 0) {
+            soma += x;
+            x = sc.nextInt();
+        }
+
+        System.out.println(soma);
+        sc.close();
     }
 }
