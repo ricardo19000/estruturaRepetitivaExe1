e87e4cd735cf9e357b500e4d4715ab7266310934
O Tal do Ricardo <33401083+ricardo19000@users.noreply.github.com>
2025-07-15 23:54:22 -0300

    Solução Exercicio 2 pt. 2

--- src/exercicios2/Exercicio2.java
+++ src/exercicios2/Exercicio2.java
@@ -1,7 +1,26 @@
 package exercicios2;
 
+import java.util.Scanner;
+
 public class Exercicio2 {
     public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Insira a quantidade de números");
+        int N = sc.nextInt();
+        int in = 0;
+        int out = 0;
+
+        for (int i = 0; i < N; i++) {
+            System.out.println("Insira o número");
+            int X = sc.nextInt();
+
+            if (10 <= X && X <= 20) {
+                in += 1;
+            } else {
+                out += 1;
+            }
+        }
+        System.out.printf("%d in\n%d out", in, out);
     }
 }
