d14e99496b1ea2062cca1cbb7516fd62fbc3cf43
O Tal do Ricardo <33401083+ricardo19000@users.noreply.github.com>
2025-07-15 23:39:19 -0300

    Solução Exercicio 1 pt. 2

--- src/exercicios2/Exercicio1.java
+++ src/exercicios2/Exercicio1.java
@@ -1,7 +1,18 @@
 package exercicios2;
 
+import java.util.Scanner;
+
 public class Exercicio1 {
     public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insira o número");
+        int x = sc.nextInt();
 
+        for (int i = 0; i < x && 1 <= x && x <= 1000; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+        sc.close();
     }
 }
